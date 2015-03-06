64e991f388756e3adb02e614a5a7faeb82c6b0c7
perlancar (@pc-office) <perlancar@gmail.com>
2015-03-06 13:39:33 +0700

    [doc] Tweak description & mention App::UpfUtils

--- lib/App/upf.pm
+++ lib/App/upf.pm
@@ -13,8 +13,16 @@ use 5.010001;
 See L<upf>.
 
 
+=head1 DESCRIPTION
+
+This distribution includes L<upf>, a single CLI scripts with subcommands.
+
+
 =head1 SEE ALSO
 
 L<Unix::Passwd::File>
+
+L<App::UpfUtils>, where each function in Unix::Passwd::File has its own CLI
+script instead of a single CLI program for all the functions like C<upf>.
 
 =cut
