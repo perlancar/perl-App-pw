c3452f39ee3fa8937ac342891a514270c7c0dc18
perlancar (@pc-office) <perlancar@gmail.com>
2015-03-06 10:25:48 +0700

    Initial commit

---
+++ lib/App/upf.pm
@@ -0,0 +1,20 @@
+package App::upf;
+
+# DATE
+# VERSION
+
+use 5.010001;
+
+1;
+# ABSTRACT: CLI interface for Unix::Passwd::File
+
+=head1 SYNOPSIS
+
+See L<upf>.
+
+
+=head1 SEE ALSO
+
+L<Unix::Passwd::File>
+
+=cut
